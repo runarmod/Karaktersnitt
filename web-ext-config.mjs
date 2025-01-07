ca3f404b3b8bad197dd3ea2bbf9268c9c4c492c6
Runar Saur Modahl <runarmod@gmail.com>
2025-01-08 00:17:50 +0100

    fix: ignore all example images and icon.svg from build
    In the v1.3.0 release, assets/example_extra_grade.png was accidentaly included in the built package, making the zip 192kB instead of 20kB.

--- web-ext-config.mjs
+++ web-ext-config.mjs
@@ -1,3 +1,9 @@
 // Ignore files which does not need to be in the built package to reduce the size of the package.
 
-export const ignoreFiles = ["config.js", "assets/example_grades.png", "README.md", ".env"];
+export const ignoreFiles = [
+  "config.js",
+  "assets/example_*.png",
+  "asserts/icon.svg",
+  "README.md",
+  ".env",
+];
